208fa3428c63edf1c5b0e8b735c6a5a7cdc0ed18
IIVACUUMII <54834869+IIVACUUMII@users.noreply.github.com>
2019-09-09 22:58:46 +0300

    Lab 1

---
+++ Lab 1/Lab 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double result = 2 * Math.Pow(x, 4) - 3 * Math.Pow(x, 3) + 4 * Math.Pow(x, 2) - 5 * x + 6;
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+        }
+    }
+}
