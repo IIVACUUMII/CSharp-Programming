00fd2851eaf70066148ecd3937e2f46ca8a948c0
IIVACUUMII <54834869+IIVACUUMII@users.noreply.github.com>
2020-06-17 19:45:58 +0300

    Create Btn.cs

---
+++ Sem 2/Lab 3/Lab 3 Sem 2/Btn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_Sem_2
+{
+    class Btn
+    {
+        public string Name { get; set; }
+
+        public Btn(string name)
+        {
+            Name = name;
+        }
+    }
+}
